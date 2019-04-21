3d44d5ae034f8d0bcc9c84e867e5396eb53041d5
Ngoc Linh Pham <pnlinh1207@gmail.com>
2019-04-21 10:04:06 +0700

    Lesson migrate and seed

---
+++ database/seeds/LessonsTableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Lesson;
+use Faker\Factory as Faker;
+use Illuminate\Database\Seeder;
+
+class LessonsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Lesson::truncate();
+
+        $faker = Faker::create();
+
+        foreach (range(1, 30) as $index) {
+            Lesson::create([
+                'title' => $faker->sentence(5),
+                'body' => $faker->paragraph(4),
+            ]);
+        }
+    }
+}
